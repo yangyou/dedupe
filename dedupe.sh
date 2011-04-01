b182c08fe31b8fbae12e3d00cbf1a476468d1bde
Jeff Lawson <jeff@bovine.net>
2011-04-01 12:13:57 -0500

    use xargs to reduce number of execs

--- dedupe.sh
+++ dedupe.sh
@@ -3,7 +3,7 @@
 BASEDIR="."
 HASHFILE="/tmp/hashfile$$"
 
-find $BASEDIR -type f -exec md5 \{\} \; > $HASHFILE
+find $BASEDIR -type f -print0 | xargs -0 md5 > $HASHFILE
 
 for HASH in `cut -d= -f2 $HASHFILE | sort | uniq -c | tr -s ' ' | egrep -v "^ 1 " | cut -d' ' -f3`; do
     echo "Duplicate hash $HASH found"
