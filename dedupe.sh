65cef9673c6c19eaff4847a719af664ef70569dc
Yang You <yang4you@gmail.com>
2011-04-01 01:29:43 -0500

    initial checkin

---
+++ dedupe.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BASEDIR="."
+HASHFILE="/tmp/hashfile$$"
+
+find $BASEDIR -type f -exec md5 \{\} \; > $HASHFILE
+
+for HASH in `cut -d= -f2 $HASHFILE | sort | uniq -c | tr -s ' ' | egrep -v "^ 1 " | cut -d' ' -f3`; do
+    echo "Duplicate hash $HASH found"
+    grep $HASH $HASHFILE
+done
+
+rm -f $HASHFILE
