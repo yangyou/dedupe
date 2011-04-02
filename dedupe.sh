3833b16658e010f1a7a99c5adb7bc2dac4369546
Yang You <yang4you@gmail.com>
2011-04-02 10:25:12 -0500

    use awk and hardcoded char counts instead of regex for possible better performance

--- dedupe.sh
+++ dedupe.sh
@@ -7,8 +7,8 @@ find $BASEDIR -type f -exec md5 \{\} \; > $HASHFILE
 
 for HASH in `cut -d= -f2 $HASHFILE | sort | uniq -c | tr -s ' ' | egrep -v "^ 1 " | cut -d' ' -f3`; do
     #echo "Duplicate hash $HASH found"
-    grep $HASH $HASHFILE | tail -n 1 | xargs | sed 's/MD5 (//;s/)[^)]*$//' 
-	#colrm 1 5 | cut -d') = ' -f1
+    grep $HASH $HASHFILE | tail -n 1 | xargs | awk '{print substr($0,6,length($0)-41)}'
+	#sed 's/MD5 (//;s/)[^)]*$//' 
 done
 
 rm -f $HASHFILE
