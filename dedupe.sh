db001e01327b8a29a267305a7a71517003f76428
Yang You <yang4you@gmail.com>
2011-04-02 01:34:03 -0500

    extract the file name and be ready to pass to rm command

--- dedupe.sh
+++ dedupe.sh
@@ -6,8 +6,9 @@ HASHFILE="/tmp/hashfile$$"
 find $BASEDIR -type f -exec md5 \{\} \; > $HASHFILE
 
 for HASH in `cut -d= -f2 $HASHFILE | sort | uniq -c | tr -s ' ' | egrep -v "^ 1 " | cut -d' ' -f3`; do
-    echo "Duplicate hash $HASH found"
-    grep $HASH $HASHFILE
+    #echo "Duplicate hash $HASH found"
+    grep $HASH $HASHFILE | tail -n 1 | xargs | sed 's/MD5 (//;s/)[^)]*$//' 
+	#colrm 1 5 | cut -d') = ' -f1
 done
 
 rm -f $HASHFILE
